c8091afcda5a6c1e9fda583c9ec2b150a2cb4391
Sekret, Konstantin <konstantine.sekret@misys.com>
2019-05-28 18:55:06 +0300

    scaffold vue app with vuetify usage

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app class="grey lighten-4">    
+    <Navbar/>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+import Navbar from '@/components/navbar'
+
+export default {
+  name: 'App',
+  components: { Navbar },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, nulla excepturi atque delectus aliquid iure molestiae vel natus sint, voluptatum vitae non ex enim minus voluptates architecto. Maxime, minus voluptatibus!</p>
+    <p></p>
+  </div>
+</template>
+
+<script>  
+
+  export default {
+    components: {
+      
+    }
+  }
+</script>
